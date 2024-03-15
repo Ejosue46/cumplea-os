fb5d24c93b99855ffdd5e84520c0433bb2bc85ab
Ejosue46 <124466405+Ejosue46@users.noreply.github.com>
2024-03-15 01:41:20 -0600

    ssss

--- assets/fireworks.js
+++ assets/fireworks.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const container = document.getElementById('contenedor-de-fuegos-artificiales'); // Corregido
+    const container = document.getElementById('fireworks-container');
 
     function createFirework() {
         const firework = document.createElement('div');
@@ -33,4 +33,30 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("openCartaBtn").addEventListener("click", function() {
         window.location.href = "carta.html";
     });
+    
+
 });
+const fuegosArtificiales = document.getElementById("fuegos-artificiales");
+
+function crearFuegoArtificial() {
+  const fuegoArtificial = document.createElement("div");
+  fuegoArtificial.classList.add("fuego-artificial");
+
+  // Posición aleatoria
+  const x = Math.random() * 100;
+  const y = Math.random() * 100;
+  fuegoArtificial.style.left = `${x}%`;
+  fuegoArtificial.style.top = `${y}%`;
+
+  // Color aleatorio
+  const color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+  fuegoArtificial.style.backgroundColor = color;
+
+  fuegosArtificiales.appendChild(fuegoArtificial);
+
+  setTimeout(() => {
+    fuegoArtificial.remove();
+  }, 2000);
+}
+
+setInterval(crearFuegoArtificial, 100);
